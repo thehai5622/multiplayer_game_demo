e409ad0c3b299aeeffad3807ecd6c72e50dbf9fb
hailt <luonghai5622@gmail.com>
2024-09-30 16:15:20 +0700

    Change name

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -11,24 +11,24 @@ canvas.height = innerHeight
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const players = {}
+const frontEndplayers = {}
 
-socket.on('updatePlayers', (backendPlayers) => {
-  for (const id in backendPlayers) {
-    const backendPlayer = backendPlayers[id]
+socket.on('updatePlayers', (backEndPlayers) => {
+  for (const id in backEndPlayers) {
+    const backEndPlayer = backEndPlayers[id]
 
-    if (!players[id]) {
-      players[id] = new Player({
-        x: backendPlayer.x,
-        y: backendPlayer.y,
+    if (!frontEndplayers[id]) {
+      frontEndplayers[id] = new Player({
+        x: backEndPlayer.x,
+        y: backEndPlayer.y,
         radius: 10,
         color: 'hsl(0, 100%, 50%)'
       })
     }
 
-    for (const id in players) {
-      if (!backendPlayers[id]) {
-        delete players[id]
+    for (const id in frontEndplayers) {
+      if (!backEndPlayers[id]) {
+        delete frontEndplayers[id]
       }
     }
   }
@@ -41,9 +41,9 @@ function animate() {
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  for (const id in players) {
-    const player = players[id]
-    player.draw()
+  for (const id in frontEndplayers) {
+    const frontEndPlayer = frontEndplayers[id]
+    frontEndPlayer.draw()
   }
 }
 
